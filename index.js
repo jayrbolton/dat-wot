8d048dfd376643dd4b177b8f3d84d995837d0b4b
Jay R Bolton <jayrbolton@gmail.com>
2017-04-09 19:34:35 -0700

    wip

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const setup = require('./lib/setup')
 const load = require('./lib/load')
+const follow = require('./lib/follow')
+const create = require('./lib/create')
 
-module.exports = {setup, load}
+module.exports = {setup, load, follow, create}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,59 @@
+const test = require('tape')
 const fs = require('fs')
 const assert = require('assert')
-const {setup, load} = require('../')
+const createDir = require('../lib/create-dir')
+const {setup, load, follow, create} = require('../')
 
-setup({path: '/tmp/x', name: 'jay', email: 'arst@example.com', passphrase: 'arstarst'})
-assert(fs.existsSync('/tmp/x'))
+const prefix = '/tmp/metadat-test'
+createDir(prefix)
 
-// process.exit()
+test('setup', (t) => {
+  setup({path: prefix + '/setup-test', name: 'jay', passphrase: 'arstarst', numBits: 512}, function(user) {
+    // Test creation of all files
+    t.assert(fs.existsSync('/tmp/metadat-test/setup-test'), 'Creates parent directory')
+    t.assert(fs.existsSync('/tmp/metadat-test/setup-test/.keys/private.key'), 'creates private key file')
+    t.assert(fs.existsSync('/tmp/metadat-test/setup-test/.keys/public.key'), 'creates public key file')
+    t.assert(fs.existsSync('/tmp/metadat-test/setup-test/jay'), 'creates user directory')
+    t.assert(fs.existsSync('/tmp/metadat-test/setup-test/jay/.dat'), 'user directory is a dat')
+    user.publicMetadat.close()
+    t.end()
+  })
+})
+
+test('load', (t) => {
+  // Test load dat
+  setup({path: prefix + '/load-test', name: 'jay', passphrase: 'arstarst', numBits: 512}, function(user) {
+    user.publicMetadat.close(function() {
+      load(prefix + '/load-test', 'arstarst', (user) => {
+        t.assert(user.publicKeyArmored, 'retrieves pubkey')
+        t.assert(user.publicMetadat, 'retrieves pubkey')
+        t.deepEqual(user.publicDats, [], 'public dat list')
+        t.deepEqual(user.relationships, [], 'relationship list')
+        t.deepEqual(user.follows, [], 'follows list')
+        t.end()
+      })
+    })
+  })
+})
+
+test.only('create.public', (t) => {
+  setup({path: prefix + '/create.public', name: 'jay', passphrase: 'arstarst', numBits: 512}, (user) => {
+    create(user, {name: 'test', public: true}, (metadat) => {
+    })
+  })
+})
+
+test('follow', (t) => {
+  const path = prefix + '/follow-test'
+  createDir(path)
+  setup({path: path + '/u1', name: 'u1', passphrase: 'arstarst', numBits: 512}, function(u1) {
+    setup({path: path + '/u2', name: 'u2', passphrase: 'arstarst', numBits: 512}, function(u2) {
+      follow(u1, u2, () => {
+        t.assert(fs.existsSync(path + '/u1/u2/.dat'))
+        u1.publicMetadat.close()
+        u2.publicMetadat.close()
+        t.end()
+      })
+    })
+  })
+})
