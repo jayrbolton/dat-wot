5d9506bdcd12982d36a420db449112eb66aba02a
Jay R Bolton <jayrbolton@gmail.com>
2017-08-15 16:53:38 -0700

    Mark dats under the user object as public true/false

--- test/createPublicDat.js
+++ test/createPublicDat.js
@@ -6,19 +6,21 @@ const {makeDatPublic, createDat, setup, close} = require('../')
 const path = './test/tmp/createPublicDat/'
 const datName = 'public-test-dat'
 
-test('create public dat', (t) => {
+test.only('create public dat', (t) => {
   setup({path, name: 'finn', pass: 'arstarst'}, (err, user) => {
     if (err) throw err
     // Create a new empty & private dat
     createDat(user, datName, (err, user, dat) => {
       if (err) throw err
       t.assert(fs.existsSync(path + '/dats/' + datName + '/.dat'))
+      t.strictEqual(user.dats[datName].public, false, 'dat is not marked as public initially')
       // Make the dat publicly listed
       makeDatPublic(user, datName, (err, user) => {
         if (err) throw err
         t.assert(user.dats[datName], 'sets an entry for the dat on the user')
         t.strictEqual(user.dats[datName].key, dat.key.toString('hex'), 'saves the dat key on the user')
         t.equal(user.dats[datName].instance, dat, 'saves the dat instance on the user')
+        t.assert(user.dats[datName].public, 'marks the dat under the user as public')
         json.read(path + '/public/dats.json', (err, dats) => {
           if (err) throw err
           t.deepEqual(dats['public-test-dat'].key, dat.key.toString('hex'), 'saves the dat key to their public dats.json')
