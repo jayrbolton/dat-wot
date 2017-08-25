eed9003e9f9e8c0e0f01ae381839fd15d5dffe5f
Jay R Bolton <jayrbolton@gmail.com>
2017-08-24 21:44:45 -0700

    Do the codes

--- test/createPublicDat.js
+++ test/createPublicDat.js
@@ -10,16 +10,15 @@ test('create public dat', (t) => {
   setup({path, name: 'finn', pass: 'arstarst'}, (err, user) => {
     if (err) throw err
     // Create a new empty & private dat
-    createDat(user, datName, (err, user, dat) => {
+    createDat(user, datName, (err, dat) => {
       if (err) throw err
       t.assert(fs.existsSync(path + '/dats/' + datName + '/.dat'))
+      t.assert(user.dats[datName], 'sets an entry for the dat on the user')
       t.strictEqual(user.dats[datName].public, false, 'dat is not marked as public initially')
+      t.equal(user.dats[datName].instance, dat, 'saves the dat instance on the user')
       // Make the dat publicly listed
-      makeDatPublic(user, datName, (err, user) => {
+      makeDatPublic(user, datName, (err) => {
         if (err) throw err
-        t.assert(user.dats[datName], 'sets an entry for the dat on the user')
-        t.strictEqual(user.dats[datName].key, dat.key.toString('hex'), 'saves the dat key on the user')
-        t.equal(user.dats[datName].instance, dat, 'saves the dat instance on the user')
         t.assert(user.dats[datName].public, 'marks the dat under the user as public')
         json.read(path + '/public/dats.json', (err, dats) => {
           if (err) throw err
